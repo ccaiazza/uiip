08e9fbb8c03f235ebdd3578b3c03f77377706282
Samsol <samanthasole94@gmail.com>
2019-03-01 17:38:19 +0100

    my hospedal

---
+++ hospital/hospitalcockpits/gensrc/org/training/hospital/cockpits/constants/GeneratedHospitalCockpitsConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 1-mar-2019 13.23.07                         ---
+ * ----------------------------------------------------------------
+ */
+package org.training.hospital.cockpits.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedHospitalCockpitsConstants
+{
+	public static final String EXTENSIONNAME = "hospitalcockpits";
+	
+	protected GeneratedHospitalCockpitsConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
---
+++ hospital/hospitalfulfilmentprocess/gensrc/org/training/hospital/fulfilmentprocess/constants/GeneratedHospitalFulfilmentProcessConstants.java
@@ -0,0 +1,33 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 1-mar-2019 13.23.07                         ---
+ * ----------------------------------------------------------------
+ */
+package org.training.hospital.fulfilmentprocess.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedHospitalFulfilmentProcessConstants
+{
+	public static final String EXTENSIONNAME = "hospitalfulfilmentprocess";
+	public static class Attributes
+	{
+		public static class ConsignmentProcess
+		{
+			public static final String DONE = "done".intern();
+			public static final String WAITINGFORCONSIGNMENT = "waitingForConsignment".intern();
+			public static final String WAREHOUSECONSIGNMENTSTATE = "warehouseConsignmentState".intern();
+		}
+	}
+	
+	protected GeneratedHospitalFulfilmentProcessConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
