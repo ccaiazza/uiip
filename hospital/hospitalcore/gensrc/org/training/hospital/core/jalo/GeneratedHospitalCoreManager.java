0ac52e72d8df2c5783183518b733ff4f81a5b6c4
Samsol <samanthasole94@gmail.com>
2019-03-01 17:16:26 +0100

    my hospital

---
+++ hospital/hospitalcore/gensrc/org/training/hospital/core/jalo/GeneratedHospitalCoreManager.java
@@ -0,0 +1,238 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 1-mar-2019 13.23.07                         ---
+ * ----------------------------------------------------------------
+ */
+package org.training.hospital.core.jalo;
+
+import de.hybris.platform.jalo.Item;
+import de.hybris.platform.jalo.Item.AttributeMode;
+import de.hybris.platform.jalo.JaloBusinessException;
+import de.hybris.platform.jalo.JaloSystemException;
+import de.hybris.platform.jalo.SessionContext;
+import de.hybris.platform.jalo.extension.Extension;
+import de.hybris.platform.jalo.type.ComposedType;
+import de.hybris.platform.jalo.type.JaloGenericCreationException;
+import java.util.HashMap;
+import java.util.Map;
+import org.training.hospital.core.constants.HospitalCoreConstants;
+import org.training.hospital.core.jalo.ApparelProduct;
+import org.training.hospital.core.jalo.ApparelSizeVariantProduct;
+import org.training.hospital.core.jalo.ApparelStyleVariantProduct;
+import org.training.hospital.core.jalo.ElectronicsColorVariantProduct;
+import org.training.hospital.core.jalo.Hospital;
+
+/**
+ * Generated class for type <code>HospitalCoreManager</code>.
+ */
+@SuppressWarnings({"deprecation","unused","cast","PMD"})
+public abstract class GeneratedHospitalCoreManager extends Extension
+{
+	protected static final Map<String, Map<String, AttributeMode>> DEFAULT_INITIAL_ATTRIBUTES;
+	static
+	{
+		final Map<String, Map<String, AttributeMode>> ttmp = new HashMap();
+		DEFAULT_INITIAL_ATTRIBUTES = ttmp;
+	}
+	@Override
+	public Map<String, AttributeMode> getDefaultAttributeModes(final Class<? extends Item> itemClass)
+	{
+		Map<String, AttributeMode> ret = new HashMap<>();
+		final Map<String, AttributeMode> attr = DEFAULT_INITIAL_ATTRIBUTES.get(itemClass.getName());
+		if (attr != null)
+		{
+			ret.putAll(attr);
+		}
+		return ret;
+	}
+	
+	public ApparelProduct createApparelProduct(final SessionContext ctx, final Map attributeValues)
+	{
+		try
+		{
+			ComposedType type = getTenant().getJaloConnection().getTypeManager().getComposedType( HospitalCoreConstants.TC.APPARELPRODUCT );
+			return (ApparelProduct)type.newInstance( ctx, attributeValues );
+		}
+		catch( JaloGenericCreationException e)
+		{
+			final Throwable cause = e.getCause();
+			throw (cause instanceof RuntimeException ?
+			(RuntimeException)cause
+			:
+			new JaloSystemException( cause, cause.getMessage(), e.getErrorCode() ) );
+		}
+		catch( JaloBusinessException e )
+		{
+			throw new JaloSystemException( e ,"error creating ApparelProduct : "+e.getMessage(), 0 );
+		}
+	}
+	
+	public ApparelProduct createApparelProduct(final Map attributeValues)
+	{
+		return createApparelProduct( getSession().getSessionContext(), attributeValues );
+	}
+	
+	public ApparelSizeVariantProduct createApparelSizeVariantProduct(final SessionContext ctx, final Map attributeValues)
+	{
+		try
+		{
+			ComposedType type = getTenant().getJaloConnection().getTypeManager().getComposedType( HospitalCoreConstants.TC.APPARELSIZEVARIANTPRODUCT );
+			return (ApparelSizeVariantProduct)type.newInstance( ctx, attributeValues );
+		}
+		catch( JaloGenericCreationException e)
+		{
+			final Throwable cause = e.getCause();
+			throw (cause instanceof RuntimeException ?
+			(RuntimeException)cause
+			:
+			new JaloSystemException( cause, cause.getMessage(), e.getErrorCode() ) );
+		}
+		catch( JaloBusinessException e )
+		{
+			throw new JaloSystemException( e ,"error creating ApparelSizeVariantProduct : "+e.getMessage(), 0 );
+		}
+	}
+	
+	public ApparelSizeVariantProduct createApparelSizeVariantProduct(final Map attributeValues)
+	{
+		return createApparelSizeVariantProduct( getSession().getSessionContext(), attributeValues );
+	}
+	
+	public ApparelStyleVariantProduct createApparelStyleVariantProduct(final SessionContext ctx, final Map attributeValues)
+	{
+		try
+		{
+			ComposedType type = getTenant().getJaloConnection().getTypeManager().getComposedType( HospitalCoreConstants.TC.APPARELSTYLEVARIANTPRODUCT );
+			return (ApparelStyleVariantProduct)type.newInstance( ctx, attributeValues );
+		}
+		catch( JaloGenericCreationException e)
+		{
+			final Throwable cause = e.getCause();
+			throw (cause instanceof RuntimeException ?
+			(RuntimeException)cause
+			:
+			new JaloSystemException( cause, cause.getMessage(), e.getErrorCode() ) );
+		}
+		catch( JaloBusinessException e )
+		{
+			throw new JaloSystemException( e ,"error creating ApparelStyleVariantProduct : "+e.getMessage(), 0 );
+		}
+	}
+	
+	public ApparelStyleVariantProduct createApparelStyleVariantProduct(final Map attributeValues)
+	{
+		return createApparelStyleVariantProduct( getSession().getSessionContext(), attributeValues );
+	}
+	
+	public ElectronicsColorVariantProduct createElectronicsColorVariantProduct(final SessionContext ctx, final Map attributeValues)
+	{
+		try
+		{
+			ComposedType type = getTenant().getJaloConnection().getTypeManager().getComposedType( HospitalCoreConstants.TC.ELECTRONICSCOLORVARIANTPRODUCT );
+			return (ElectronicsColorVariantProduct)type.newInstance( ctx, attributeValues );
+		}
+		catch( JaloGenericCreationException e)
+		{
+			final Throwable cause = e.getCause();
+			throw (cause instanceof RuntimeException ?
+			(RuntimeException)cause
+			:
+			new JaloSystemException( cause, cause.getMessage(), e.getErrorCode() ) );
+		}
+		catch( JaloBusinessException e )
+		{
+			throw new JaloSystemException( e ,"error creating ElectronicsColorVariantProduct : "+e.getMessage(), 0 );
+		}
+	}
+	
+	public ElectronicsColorVariantProduct createElectronicsColorVariantProduct(final Map attributeValues)
+	{
+		return createElectronicsColorVariantProduct( getSession().getSessionContext(), attributeValues );
+	}
+	
+	public Hospital createHead(final SessionContext ctx, final Map attributeValues)
+	{
+		try
+		{
+			ComposedType type = getTenant().getJaloConnection().getTypeManager().getComposedType( HospitalCoreConstants.TC.HEAD );
+			return (Hospital)type.newInstance( ctx, attributeValues );
+		}
+		catch( JaloGenericCreationException e)
+		{
+			final Throwable cause = e.getCause();
+			throw (cause instanceof RuntimeException ?
+			(RuntimeException)cause
+			:
+			new JaloSystemException( cause, cause.getMessage(), e.getErrorCode() ) );
+		}
+		catch( JaloBusinessException e )
+		{
+			throw new JaloSystemException( e ,"error creating Head : "+e.getMessage(), 0 );
+		}
+	}
+	
+	public Hospital createHead(final Map attributeValues)
+	{
+		return createHead( getSession().getSessionContext(), attributeValues );
+	}
+	
+	public Hospital createHospital(final SessionContext ctx, final Map attributeValues)
+	{
+		try
+		{
+			ComposedType type = getTenant().getJaloConnection().getTypeManager().getComposedType( HospitalCoreConstants.TC.HOSPITAL );
+			return (Hospital)type.newInstance( ctx, attributeValues );
+		}
+		catch( JaloGenericCreationException e)
+		{
+			final Throwable cause = e.getCause();
+			throw (cause instanceof RuntimeException ?
+			(RuntimeException)cause
+			:
+			new JaloSystemException( cause, cause.getMessage(), e.getErrorCode() ) );
+		}
+		catch( JaloBusinessException e )
+		{
+			throw new JaloSystemException( e ,"error creating Hospital : "+e.getMessage(), 0 );
+		}
+	}
+	
+	public Hospital createHospital(final Map attributeValues)
+	{
+		return createHospital( getSession().getSessionContext(), attributeValues );
+	}
+	
+	public Hospital createReparto(final SessionContext ctx, final Map attributeValues)
+	{
+		try
+		{
+			ComposedType type = getTenant().getJaloConnection().getTypeManager().getComposedType( HospitalCoreConstants.TC.REPARTO );
+			return (Hospital)type.newInstance( ctx, attributeValues );
+		}
+		catch( JaloGenericCreationException e)
+		{
+			final Throwable cause = e.getCause();
+			throw (cause instanceof RuntimeException ?
+			(RuntimeException)cause
+			:
+			new JaloSystemException( cause, cause.getMessage(), e.getErrorCode() ) );
+		}
+		catch( JaloBusinessException e )
+		{
+			throw new JaloSystemException( e ,"error creating Reparto : "+e.getMessage(), 0 );
+		}
+	}
+	
+	public Hospital createReparto(final Map attributeValues)
+	{
+		return createReparto( getSession().getSessionContext(), attributeValues );
+	}
+	
+	@Override
+	public String getName()
+	{
+		return HospitalCoreConstants.EXTENSIONNAME;
+	}
+	
+}
