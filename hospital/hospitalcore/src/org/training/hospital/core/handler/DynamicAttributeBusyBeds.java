79b9da2f3c18c528f2bce0dbdeb6f5f8b4a2e848
TAMMARO Cinzia <cinzia.tammaro@soprasteria.com>
2019-03-27 10:22:38 +0100

    BusyBed

---
+++ hospital/hospitalcore/src/org/training/hospital/core/handler/DynamicAttributeBusyBeds.java
@@ -0,0 +1,47 @@
+/**
+ *
+ */
+package org.training.hospital.core.handler;
+
+import de.hybris.platform.servicelayer.model.attribute.DynamicAttributeHandler;
+
+import org.training.hospital.core.model.RepartoModel;
+
+/**
+ * @author ctammaro
+ *
+ */
+public class DynamicAttributeBusyBeds implements DynamicAttributeHandler<String, RepartoModel>
+{
+
+
+	@Override
+	public String get(final RepartoModel item)
+	{
+
+		if (item == null)
+		{
+			throw new IllegalArgumentException("RepartoModel model is required");
+		}
+
+		else
+		{
+			if (item.getPatients() == null)
+			{
+				return 0 + " ";
+			}
+			else
+			{
+				return item.getPatients().size() + " ";
+			}
+		}
+	}
+
+
+	@Override
+	public void set(final RepartoModel arg0, final String arg1)
+	{
+
+	}
+
+}
