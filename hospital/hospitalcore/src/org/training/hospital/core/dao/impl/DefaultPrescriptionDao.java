adecdb02417ff09a5381325b594c45d9c3c24383
TAMMARO Cinzia <cinzia.tammaro@soprasteria.com>
2019-07-15 16:04:48 +0200

    Update Prescription

--- hospital/hospitalcore/src/org/training/hospital/core/dao/impl/DefaultPrescriptionDao.java
+++ hospital/hospitalcore/src/org/training/hospital/core/dao/impl/DefaultPrescriptionDao.java
@@ -1,6 +1,9 @@
 package org.training.hospital.core.dao.impl;
 
+import java.util.List;
+
 import org.training.hospital.core.dao.PrescriptionDao;
+import org.training.hospital.core.model.BedModel;
 import org.training.hospital.core.model.PrescriptionModel;
 import org.training.hospital.core.model.RoomModel;
 
@@ -13,21 +16,36 @@ public class DefaultPrescriptionDao extends DefaultGenericDao implements Prescri
 
 	public DefaultPrescriptionDao(String typecode) {
 		super(typecode);
-		
+
 	}
 	@Override
 	public PrescriptionModel findPrescriptionByCode(String code)  {
-		
+
 		final String queryString = " SELECT {"+ PrescriptionModel.PK + "} FROM {"+ PrescriptionModel._TYPECODE + "} WHERE {"+ PrescriptionModel.CODE + "}=?code " ;
 		final FlexibleSearchQuery query= new FlexibleSearchQuery(queryString);
 		query.addQueryParameter("code",code);
 		final SearchResult<PrescriptionModel> result = getFlexibleSearchService().search(query);
 		if(result.getResult().isEmpty()) {
-	
-		return null;
+
+			return null;
 		}
 		else {	return result.getResult().get(0);
 		}
+	}
+	@Override
+	public List<PrescriptionModel> findPrescriptionByPatient(String code) {
+		final String queryString= "SELECT {" +PrescriptionModel.PK +"} FROM {PRESCRIPTION AS P JOIN PATIENT AS C ON{"+ PrescriptionModel.PATIENT +"} = {C.PK}} WHERE{C.UID} =?code";
+
+		final FlexibleSearchQuery query=new FlexibleSearchQuery(queryString);
+		query.addQueryParameter("code", code);
+		final SearchResult<PrescriptionModel> result=getFlexibleSearchService().search(query);
+		if(result.getResult().isEmpty()) {
+
+			return null;
+		}
+		else {	return result.getResult();
 		}
 	}
+}
+
 
---
+++ hospital/hospitalfacades/src/org/training/hospital/facades/populators/PrescriptionPopulator.java
@@ -0,0 +1,75 @@
+package org.training.hospital.facades.populators;
+
+import org.springframework.beans.factory.annotation.Required;
+import org.training.hospital.core.model.PatientModel;
+import org.training.hospital.core.model.PrescriptionModel;
+import org.training.hospital.facades.product.data.PatientData;
+import org.training.hospital.facades.product.data.PrescriptionData;
+
+import de.hybris.platform.commercefacades.product.data.ProductData;
+import de.hybris.platform.converters.Populator;
+import de.hybris.platform.core.model.product.ProductModel;
+import de.hybris.platform.servicelayer.dto.converter.Converter;
+import de.hybris.platform.servicelayer.dto.converter.ConversionException;
+
+public class PrescriptionPopulator implements Populator<PrescriptionModel, PrescriptionData> {
+
+	
+	private Converter<PatientModel, PatientData> patientConverter;
+	private Converter<ProductModel, ProductData> productConverter;
+	
+	
+	@Override
+	public void populate(PrescriptionModel source, PrescriptionData target) throws ConversionException {
+	
+		if (source == null)
+		{
+			throw new IllegalArgumentException("Model is empty");
+		}
+		else
+		{
+			target.setCode((source.getCode()));
+			target.setAsl(source.getAsl());
+			target.setDistrict(source.getDistrict());
+			target.setDate(source.getDate());
+			target.setExemption(source.getExemption());
+			target.setPatient(patientConverter.convert(source.getPatient()));
+			target.setProducts(productConverter.convertAll(source.getProducts()));
+			
+		}
+
+		
+		
+		
+	}
+
+
+	
+	public Converter<PatientModel, PatientData> getPatientConverter() {
+		return patientConverter;
+	}
+
+
+	@Required
+	public void setPatientConverter(Converter<PatientModel, PatientData> patientConverter) {
+		this.patientConverter = patientConverter;
+	}
+
+
+
+
+	public Converter<ProductModel, ProductData> getProductConverter() {
+		return productConverter;
+	}
+
+
+	@Required
+	public void setProductConverter(Converter<ProductModel, ProductData> productConverter) {
+		this.productConverter = productConverter;
+	}
+
+		
+		
+	
+
+}
